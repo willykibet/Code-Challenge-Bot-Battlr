b5fb90fa45355b6f799bcada91a9f67cf6b1bac3
Willy <willykipchumba22@gmail.com>
2023-11-06 22:48:11 +0300

    mh

--- bot-battlr/src/App.js
+++ bot-battlr/src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./components/Navbar";
+import Botshow from "./components/Botshow";
+import { useEffect, useState } from "react";
+import BotCollection from "./components/BotCollection";
 function App() {
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/bots")
+      .then((response) => response.json())
+      .then((bots) => {
+        setBots(bots);
+      });
+  }, []);
+
+  function enlistBot(bot) {
+    setBots(bots.map((b) => (b.id === bot.id ? { ...b, army: true } : b)));
+  }
+
+  function removeBot(bot) {
+    setBots(bots.map((b) => (b.id === bot.id ? { ...b, army: false } : b)));
+  }
+
+  function deleteBot(bot) {
+    const deletedBot = bots.filter((b) => b.id !== bot.id);
+    setBots((bots) => deletedBot);
+
+    // Delete in DB
+    fetch(`http://localhost:4000/bots/${bot.id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((result) => console.log(result));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <section className="jumbotron text-center">
+        <div className="container">
+          <div className="row">
+            <Botshow
+              records={bots.filter((b) => b.army)}
+              removeBot={removeBot}
+              deleteBot={deleteBot}
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row">
+            <BotCollection
+              records={bots}
+              enlistBot={enlistBot}
+              deleteBot={deleteBot}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
